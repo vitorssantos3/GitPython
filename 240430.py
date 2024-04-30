5c2cf745266ea1c93a9be4fa06444f4a14938c17
vitorssantos3 <vitorsansantos3@gmail.com>
2024-04-30 10:23:42 -0300

    No

--- 240430.py
+++ 240430.py
@@ -7,6 +7,7 @@
 #     else:
 #         print("Não encontrado")
 
+# ________ ATENÇÃO:    ipython ________ comando para o terminal
 def index (l, letra):
     for i, char in enumerate(l):
         if char ==letra:
@@ -16,3 +17,36 @@ def index (l, letra):
 l = list("knmdndnsnnn")
 letra=input("digite uma letra:")
 index(l,letra)
+
+def encontra_max(lista):
+    if len(lista)==0:
+        return None
+    maior_numero = lista[0]
+    # maior_numero = None
+    for i in lista:
+        if i > maior_numero:
+        # if maior_numero is None or i >maior_numero:
+            maior_numero=i
+    return maior_numero
+
+def encontra_min(lista):
+    menor_numero= lista[0]
+    for i in lista:
+        if i<menor_numero:
+            menor_numero =i
+    return menor_numero
+
+def ordem_crescente(lista):
+    lista_ordenada =[]
+    while len(lista):
+        i = index(encontra_min(lista))
+        lista_ordenada.append(lista.pop(i))
+    return lista_ordenada
+
+def ordena__decrescente(lista):
+    return ordem_crescente(lista)[::-1]
+
+def ordena(lista,decrescnte =False):
+    if decrescnte:
+        return ordena__decrescente(lista)
+    return ordena__decrescente(lista)
